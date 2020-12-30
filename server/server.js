a00feb09900a2db1013e8acc5242391cd78a9dca
thiagomconte <thiagomconte@hotmail.com>
2020-12-30 01:40:21 -0300

    update

--- server/server.js
+++ server/server.js
@@ -1,27 +1,38 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+//! Configuração de middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/', (req, res) => {
-    var {name} = req.body
-    res.status(200).json(name)
-})
+//! Conexao com banco de dados
+require("./dataBase/mongodbconnection");
 
-app.get('/', (req, res) => {
-    res.json('Ta funcionando')
-})
+//! Routes
+app.use("/user", require("./router/userRoute"));
+app.use("/product", require("./router/productRoute"));
+app.use("/category", require("./router/categoryRoute"));
+app.use("/owner", require("./router/ownerRoute"));
 
-const PORT = 3000 || process.env.PORT
-app.listen(PORT, (err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log(`Server running on PORT ${PORT}`)
-    }
-})
+app.post("/", (req, res) => {
+  var { name } = req.body;
+  res.status(200).json(name);
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello amazon");
+});
+
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server running on PORT ${PORT}`);
+  }
+});
